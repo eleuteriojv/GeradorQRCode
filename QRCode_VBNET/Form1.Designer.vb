a50b6a8365528f4beb3cd3312713befaa90a8eb8
joaoeleuterio <joaoeleuterio@NEAD-003>
2022-05-24 15:14:49 -0300

    Adicionar arquivos de projeto.

---
+++ QRCode_VBNET/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.picImagem = New System.Windows.Forms.PictureBox()
+        Me.txtTexto = New System.Windows.Forms.TextBox()
+        Me.btnGerarQRCode = New System.Windows.Forms.Button()
+        Me.btnSalvarQRCode = New System.Windows.Forms.Button()
+        Me.btnCarregarQRCode = New System.Windows.Forms.Button()
+        Me.btnLerQRCode = New System.Windows.Forms.Button()
+        CType(Me.picImagem, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picImagem
+        '
+        Me.picImagem.Location = New System.Drawing.Point(32, 12)
+        Me.picImagem.Name = "picImagem"
+        Me.picImagem.Size = New System.Drawing.Size(167, 134)
+        Me.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.picImagem.TabIndex = 0
+        Me.picImagem.TabStop = False
+        '
+        'txtTexto
+        '
+        Me.txtTexto.Location = New System.Drawing.Point(32, 152)
+        Me.txtTexto.Name = "txtTexto"
+        Me.txtTexto.Size = New System.Drawing.Size(167, 23)
+        Me.txtTexto.TabIndex = 1
+        '
+        'btnGerarQRCode
+        '
+        Me.btnGerarQRCode.Location = New System.Drawing.Point(32, 181)
+        Me.btnGerarQRCode.Name = "btnGerarQRCode"
+        Me.btnGerarQRCode.Size = New System.Drawing.Size(168, 44)
+        Me.btnGerarQRCode.TabIndex = 2
+        Me.btnGerarQRCode.Text = "Gerar QRCode"
+        Me.btnGerarQRCode.UseVisualStyleBackColor = True
+        '
+        'btnSalvarQRCode
+        '
+        Me.btnSalvarQRCode.Location = New System.Drawing.Point(33, 231)
+        Me.btnSalvarQRCode.Name = "btnSalvarQRCode"
+        Me.btnSalvarQRCode.Size = New System.Drawing.Size(167, 44)
+        Me.btnSalvarQRCode.TabIndex = 3
+        Me.btnSalvarQRCode.Text = "Salvar QRCode"
+        Me.btnSalvarQRCode.UseVisualStyleBackColor = True
+        '
+        'btnCarregarQRCode
+        '
+        Me.btnCarregarQRCode.Location = New System.Drawing.Point(33, 281)
+        Me.btnCarregarQRCode.Name = "btnCarregarQRCode"
+        Me.btnCarregarQRCode.Size = New System.Drawing.Size(166, 47)
+        Me.btnCarregarQRCode.TabIndex = 4
+        Me.btnCarregarQRCode.Text = "Carregar QRCode"
+        Me.btnCarregarQRCode.UseVisualStyleBackColor = True
+        '
+        'btnLerQRCode
+        '
+        Me.btnLerQRCode.Location = New System.Drawing.Point(34, 334)
+        Me.btnLerQRCode.Name = "btnLerQRCode"
+        Me.btnLerQRCode.Size = New System.Drawing.Size(165, 42)
+        Me.btnLerQRCode.TabIndex = 5
+        Me.btnLerQRCode.Text = "Ler QRCode"
+        Me.btnLerQRCode.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(231, 384)
+        Me.Controls.Add(Me.btnLerQRCode)
+        Me.Controls.Add(Me.btnCarregarQRCode)
+        Me.Controls.Add(Me.btnSalvarQRCode)
+        Me.Controls.Add(Me.btnGerarQRCode)
+        Me.Controls.Add(Me.txtTexto)
+        Me.Controls.Add(Me.picImagem)
+        Me.Name = "Form1"
+        Me.Text = "Gerador de QRCode"
+        CType(Me.picImagem, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picImagem As PictureBox
+    Friend WithEvents txtTexto As TextBox
+    Friend WithEvents btnGerarQRCode As Button
+    Friend WithEvents btnSalvarQRCode As Button
+    Friend WithEvents btnCarregarQRCode As Button
+    Friend WithEvents btnLerQRCode As Button
+End Class
---
+++ QRCode_VBNET/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports MessagingToolkit.QRCode.Codec
+Imports MessagingToolkit.QRCode.Codec.Data
+Public Class Form1
+    Private Sub btnGerarQRCode_Click(sender As Object, e As EventArgs) Handles btnGerarQRCode.Click
+        Try
+            Dim qrencod As New QRCodeEncoder
+            Dim qrcode As Bitmap = qrencod.Encode(txtTexto.Text)
+            picImagem.Image = TryCast(qrcode, Image)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnSalvarQRCode_Click(sender As Object, e As EventArgs) Handles btnSalvarQRCode.Click
+        Try
+            Dim sfd As New SaveFileDialog()
+            sfd.InitialDirectory = "c:\QrCode\"
+            sfd.Filter = "PNG | *.png| JPEG| *.jpg| BMP|*.bmp"
+            If sfd.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                picImagem.Image.Save(sfd.FileName)
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnCarregarQRCode_Click(sender As Object, e As EventArgs) Handles btnCarregarQRCode.Click
+        Try
+            Dim ofd As New OpenFileDialog()
+            ofd.InitialDirectory = "c:\QrCode\"
+            If ofd.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                picImagem.ImageLocation = ofd.FileName
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnLerQRCode_Click(sender As Object, e As EventArgs) Handles btnLerQRCode.Click
+        Try
+            Dim decoder As New QRCodeDecoder
+            MessageBox.Show(decoder.Decode(New QRCodeBitmapImage(TryCast(picImagem.Image, Bitmap))))
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
